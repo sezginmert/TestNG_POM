580203d86e0edf04c2405cfb2f206c874f166ab3
sezginmert <sezginmertt@yahoo.com>
2024-12-19 09:16:07 +0300

    day14-121924

---
+++ src/test/java/tests/day14_testNGFramework_assertions/C03_WebUniversityTest.java
@@ -0,0 +1,20 @@
+package tests.day14_testNGFramework_assertions;
+
+import org.testng.annotations.Test;
+
+public class C03_WebUniversityTest {
+
+    @Test
+    public void popupTest(){
+        //1."https://webdriveruniversity.com/" adresine gidin
+        //2."Login Portal" a kadar asagi inin
+        //3."Login Portal" a tiklayin
+        //4.Diger window'a gecin
+        //5."username" ve "password" kutularina deger yazdirin
+        //6."login" butonuna basin
+        //7.Popup'ta cikan yazinin "validation failed" oldugunu test edin
+        //8.Ok diyerek Popup'i kapatin
+        //9.Ilk sayfaya geri donun
+        //10.Ilk sayfaya donuldugunu test edin
+    }
+}
